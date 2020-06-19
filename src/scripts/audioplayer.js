b1fa1c413a03ff7928899bbff86b9e75fd5e088d
n-pola <n.polarek@live.de>
2020-06-19 12:18:00 +0200

    render template to playlist section

--- src/scripts/audioplayer.js
+++ src/scripts/audioplayer.js
@@ -1,1 +1,12 @@
 // eslint-disable-next-line import/extensions
+const playlistWrap = document.querySelector('[data-js-playlist-section]');
+fetch('./json/playlist.json')
+  .then((response) => response.json())
+  .then((data) => {
+    fetch('./templates/playlist.mustache')
+      .then((response) => response.text())
+      .then((template) => {
+        const rendered = Mustache.render(template, data);
+        playlistWrap.innerHTML = rendered;
+      });
+  });
