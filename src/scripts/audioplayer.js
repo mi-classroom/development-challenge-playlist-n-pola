2813ece2e2ed7b833fa344cd16ba2f2f3a3319fa
n-pola <n.polarek@live.de>
2020-06-19 12:47:55 +0200

    add basic function to play Songs

--- src/scripts/audioplayer.js
+++ src/scripts/audioplayer.js
@@ -1,5 +1,35 @@
+class Playlist {
+  constructor(playlist) {
+    this.playlist = playlist;
+    this.songs = playlist.querySelectorAll(playlist.dataset.jsPlaylist);
+    this.currentSong = null;
+    this.songsObjs = [];
+    this.songs.forEach((song, i) => {
+      const sound = new Howl({
+        src: [song.dataset.jsSongSrc],
+      });
+      console.log(sound);
+      this.songsObjs.push(sound);
+
+      song.addEventListener('click', () => {
+        this.play(i);
+      });
+    });
+  }
+
+  play(index) {
+    const { currentSong } = this;
+    if (currentSong != null) { this.songsObjs[currentSong].stop(); }
+    if (currentSong != index) {
+      this.songsObjs[index].play();
+      this.currentSong = index;
+    }
+  }
+}
+
 // eslint-disable-next-line import/extensions
 const playlistWrap = document.querySelector('[data-js-playlist-section]');
+playlistWrap.addEventListener;
 fetch('./json/playlist.json')
   .then((response) => response.json())
   .then((data) => {
@@ -8,5 +38,7 @@ fetch('./json/playlist.json')
       .then((template) => {
         const rendered = Mustache.render(template, data);
         playlistWrap.innerHTML = rendered;
+        const playlist = document.querySelector('[data-js-playlist]');
+        new Playlist(playlist);
       });
   });
